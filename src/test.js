96a0be1409c3eec6eff57d4ee55c79ab49d2a403
Thomas Duffin <thomaslukeduffin@hotmail.co.uk>
2022-04-04 19:08:36 +0100

    simplifying the project

--- src/test.js
+++
@@ -1,9 +0,0 @@
-// parsing url queries
-const queryString = window.location.search;
-const queryContent = new URLSearchParams(queryString).get('msg');
-
-// if we've entered a query, else this'll be null
-if (queryContent){
-    alert(queryContent);
-}
-
